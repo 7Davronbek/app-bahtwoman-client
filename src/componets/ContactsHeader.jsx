99ec368724b54a53c47c73fe8d831088b086b82f
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-14 11:29:38 +0500

    bahtwoman-app

--- src/componets/ContactsHeader.jsx
+++ src/componets/ContactsHeader.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
 const ContactsHeader = () => {
-  return (
-    <>
-        <div className="contactsHeader">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <h1>Мы с нестерпением ждём вас</h1>
+    return (
+        <>
+            <div className="contactsHeader">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 zed">
+                            <h1>Мы с нестерпением ждём вас</h1>
+                        </div>
                     </div>
                 </div>
+                <div className="blur"></div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default ContactsHeader
--- src/componets/Social.jsx
+++ src/componets/Social.jsx
@@ -5,9 +5,9 @@ const Social = () => {
     <>
       <div className="social">
         <div className="soc_icon">
-          <a href="#!"><img src="/image/tg.svg" alt="" /></a>
-          <a href="#!"><img src="/image/insta.svg" alt="" /></a>
-          <a href="#!"><img src="/image/you.svg" alt="" /></a>
+          <a target={'_blank'} rel="noreferrer" href="https://t.me/bahtwoman"><img src="/image/tg.svg" alt="" /></a>
+          <a target={'_blank'} rel="noreferrer" href="https://instagram.com/bahtwoman_uz?utm_medium=copy_link"><img src="/image/insta.svg" alt="" /></a>
+          <a target={'_blank'} rel="noreferrer" href="https://youtube.com/channel/UCsYwfTUj-fTrC8P0M5Fzq0Q"><img src="/image/you.svg" alt="" /></a>
         </div>
       </div>
     </>
