924912ab120919431c46d70cf77434aaf00c6741
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-14 17:06:31 +0500

    bahtwoman-app

--- src/componets/Weimg.jsx
+++ src/componets/Weimg.jsx
@@ -5,8 +5,11 @@ const Weimg = () => {
         <><div className="Weimg">
             <div className="container">
                 <div className="row">
-                    <div className="col-3"><img className='w-100' src="/image/we_2.png" alt="" /></div>
-                    <div className="col-5"><img className='w-100' src="/image/we_1.png" alt="" /></div>
+                    <div className="col-3">
+                        <div className='mb-4'><img className='w-100' src="/image/we_3.png" alt="" /></div>
+                        <div><img className='w-100' src="/image/we_4.png" alt="" /></div>
+                    </div>
+                    <div className="col-5"><img style={{ height: "100%", objectFit: "cover" }} className='w-100' src="/image/we_1.png" alt="" /></div>
                     <div className="col-3"><img className='w-100' src="/image/we_2.png" alt="" /></div>
                 </div>
             </div>
