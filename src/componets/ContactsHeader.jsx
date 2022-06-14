01490eee0224666833d65904f2689afdda22fa47
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-14 18:26:06 +0500

    bahtwoman-app

--- src/componets/ContactsHeader.jsx
+++ src/componets/ContactsHeader.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios"
+import { API_PATH } from '../constants/tools'
 
 const ContactsHeader = () => {
+    const [name, setName] = useState('')
+    const [phone_number, setPhone_number] = useState('')
+    const [message, setMessage] = useState('')
+    const sent = (e) => {
+        e.preventDefault()
+        axios.post(API_PATH, { name, phone_number, message })
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
     return (
         <>
             <div className="contactsHeader">
@@ -8,6 +23,12 @@ const ContactsHeader = () => {
                     <div className="row">
                         <div className="col-12 zed">
                             <h1>Мы с нестерпением ждём вас</h1>
+                            <form onSubmit={sent}>
+                                <input value={name} onChange={(e) => setName(e.target.value)} type="text" name='name' />
+                                <input value={phone_number} onChange={(e) => setPhone_number(e.target.value)} type="text" name='phone_number' />
+                                <input value={message} onChange={(e) => setMessage(e.target.value)} type="text" name='message' />
+                                <button type='submit'>Send </button>
+                            </form>
                         </div>
                     </div>
                 </div>
